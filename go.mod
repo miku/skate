2dc1ab76678ec3a67c766bb5135176f273330ac9
Martin Czygan <martin.czygan@gmail.com>
2021-03-06 00:21:09 +0100

    move test data into file

--- go.mod
+++ go.mod
@@ -4,12 +4,13 @@ go 1.15
 
 require (
 	github.com/dgraph-io/ristretto v0.0.3
-	github.com/goccy/go-json v0.4.4
 	github.com/json-iterator/go v1.1.10
 	github.com/miku/parallel v0.0.0-20210205190127-d1fa15dcea0c
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/sethgrid/pester v1.1.0
+	github.com/stretchr/testify v1.5.1 // indirect
 	golang.org/x/text v0.3.5
-	golang.org/x/tools v0.1.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
--- zip_test.go
+++ zip_test.go
@@ -3,22 +3,25 @@ package skate
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
 )
 
 func TestZipper(t *testing.T) {
-	a := strings.NewReader(`
-k0 a
-k1 b
-k2 c
-k3 d
-`)
-	b := strings.NewReader(`
-k1 B
-k3 D
-`)
+	var cases = []struct {
+		a string
+		b string
+		c string
+	}{
+		{
+			"testdata/zip/c0a",
+			"testdata/zip/c0b",
+			"testdata/zip/c0c",
+		},
+	}
 	keyFunc := func(s string) (string, error) {
 		parts := strings.Fields(s)
 		if len(parts) != 2 {
@@ -26,20 +29,25 @@ k3 D
 		}
 		return parts[0], nil
 	}
-	var buf bytes.Buffer
-	groupFunc := func(g *GroupedCluster) error {
-		fmt.Fprintf(&buf, "A=%v, B=%v ",
-			sliceMap(g.A, strings.TrimSpace),
-			sliceMap(g.B, strings.TrimSpace))
-		return nil
-	}
-	err := Zipper(a, b, keyFunc, groupFunc, ioutil.Discard)
-	if err != nil {
-		t.Errorf("failed: %v", err)
-	}
-	want := "A=[k1 b], B=[k1 B] A=[k3 d], B=[k3 D]"
-	if want != strings.TrimSpace(buf.String()) {
-		t.Errorf("got %v, want %v", buf.String(), want)
+	for _, c := range cases {
+		ar := mustReadFileReader(c.a)
+		br := mustReadFileReader(c.b)
+		cr := strings.TrimSpace(mustReadFileString(c.c))
+		var buf bytes.Buffer
+		groupFunc := func(g *GroupedCluster) error {
+			fmt.Fprintf(&buf, "A=%v, B=%v ",
+				sliceMap(g.A, strings.TrimSpace),
+				sliceMap(g.B, strings.TrimSpace))
+			return nil
+		}
+		err := Zipper(ar, br, keyFunc, groupFunc, ioutil.Discard)
+		if err != nil {
+			t.Errorf("failed: %v", err)
+		}
+		got := strings.TrimSpace(buf.String())
+		if cr != got {
+			t.Errorf("got %v, want %v", got, cr)
+		}
 	}
 }
 
@@ -48,4 +56,20 @@ func sliceMap(ss []string, f func(string) string) (result []string) {
 		result = append(result, f(v))
 	}
 	return
+}
+
+func mustReadFileReader(filename string) io.ReadCloser {
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func mustReadFileString(filename string) string {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	return string(b)
 }
