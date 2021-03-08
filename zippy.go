f058e3f465be9ecf592410454eb10671672ca611
Martin Czygan <martin.czygan@gmail.com>
2021-03-08 13:40:53 +0100

    refactor helpers

--- zippy.go
+++ zippy.go
@@ -9,17 +9,20 @@ import (
 // ZipVerifyRefs takes a release and refs reader (tsv, with ident, key, doc)
 // and will execute gf for each group found.
 func ZipVerifyRefs(releases, refs io.Reader, w io.Writer) error {
-	// Define a grouper, as closure so we can use the writer.
+	// Define a grouper, working on one set of refs and releases with the same
+	// key at a time. Here, we do verification and write out the generated
+	// biblioref.
 	grouper := func(g *Group) error {
 		if len(g.A) == 0 || len(g.B) == 0 {
 			return nil
 		}
-		pivot, err := lineToRelease(g.A[0])
+		pivot, err := lineColumnToRelease(g.A[0], "\t", 3)
+
 		if err != nil {
 			return err
 		}
 		for _, line := range g.B {
-			re, err := lineToRelease(line)
+			re, err := lineColumnToRelease(line, "\t", 3)
 			if err != nil {
 				return err
 			}
@@ -30,10 +33,16 @@ func ZipVerifyRefs(releases, refs io.Reader, w io.Writer) error {
 			}
 			// XXX: we need to assemble the final document here (as we can
 			// access both release docs here)
+			_ = generateBiblioRef(re, pivot)
 		}
 		return nil
 	}
 	return Zipper(releases, refs, getKey, grouper)
+}
+
+// generateBiblioRef generates a bibliographic schema document.
+func generateBiblioRef(source, target *Release) *BiblioRef {
+	return nil
 }
 
 func getKey(line string) (string, error) {
@@ -43,14 +52,21 @@ func getKey(line string) (string, error) {
 	}
 	return "", fmt.Errorf("unexpected input: %s", line)
 }
-func lineToRelease(line string) (*Release, error) {
-	parts := strings.Split(strings.TrimSpace(line), "\t")
-	if len(parts) == 3 {
-		var re *Release
-		if err := json.Unmarshal([]byte(parts[2]), &re); err != nil {
-			return nil, err
-		}
-		return re, nil
+
+// lineColumn returns a specific column (1-indexed, like cut) from a tabular
+// file, returns empty string if column is invalid.
+func lineColumn(line, sep string, column int) string {
+	parts := strings.Split(strings.TrimSpace(line), sep)
+	if len(parts) < column {
+		return ""
 	}
-	return nil, fmt.Errorf("unexpected input: %s", line)
+	return parts[column-1]
+}
+
+func lineColumnToRelease(line, sep string, column int) (*Release, error) {
+	var re *Release
+	if err := json.Unmarshal([]byte(lineColumn(line, sep, 3)), &re); err != nil {
+		return nil, err
+	}
+	return re, nil
 }
